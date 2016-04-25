5a89c4b4acc32c26de13e52bc09f89beee59c2c6
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2016-04-24 23:30:38 -0500

    Console for practice.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum(1, 2);
+
+            Console.WriteLine(Concatenate2("test ", "me"));
+            Concatenate("Test, ", "again");
+
+            for(int counter = 0; counter < 11; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+
+        static public void Sum(int one, int two)
+        {
+            Console.WriteLine(one + two);
+        }
+
+        static public void Concatenate(string first, string second)
+        {
+            string whole = first + second;
+            Console.WriteLine(whole);
+        }
+
+        static string Concatenate2(string first, string second)
+        {
+            string whole = first + second;
+            return whole;
+        }
+    }
+}
